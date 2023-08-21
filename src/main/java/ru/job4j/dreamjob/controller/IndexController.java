6122d141c18668be67d1f048c7c8452dd05eda6a
devseag <example@gmail.com>
2023-08-21 10:55:37 -0300

    add controller index

--- src/main/java/ru/job4j/dreamjob/controller/IndexController.java
+++ src/main/java/ru/job4j/dreamjob/controller/IndexController.java
@@ -1,14 +1,17 @@
 package ru.job4j.dreamjob.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-@RestController
+//@RestController
+@Controller
 public class IndexController {
 
     @GetMapping("/index")
     public String getIndex() {
-        return "Hello World!";
+//        return "Hello World!";
+        return "index";
     }
 
 }
